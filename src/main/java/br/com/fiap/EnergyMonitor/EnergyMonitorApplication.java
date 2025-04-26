898e5182d01d9502e663f971f1c435dec7eef393
HumbertoVitalino <130328321+HumbertoVitalino@users.noreply.github.com>
2025-04-26 13:22:36 -0300

    project start

---
+++ src/main/java/br/com/fiap/EnergyMonitor/EnergyMonitorApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.EnergyMonitor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnergyMonitorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnergyMonitorApplication.class, args);
+	}
+
+}
