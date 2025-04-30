b0a9a0997a2ddb510bc2ce868c7cea405fa1bee3
HumbertoVitalino <130328321+HumbertoVitalino@users.noreply.github.com>
2025-04-30 10:28:14 -0300

    feat: update and delete

--- src/main/java/br/com/fiap/EnergyMonitor/repository/SectorRepository.java
+++ src/main/java/br/com/fiap/EnergyMonitor/repository/SectorRepository.java
@@ -1,11 +1,12 @@
 package br.com.fiap.EnergyMonitor.repository;
 
 import br.com.fiap.EnergyMonitor.model.Sector;
-import br.com.fiap.EnergyMonitor.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface SectorRepository extends JpaRepository<Sector, Long> {
     List<Sector> findAllByUserEmail(String email);
+    Optional<Sector> findById(Long id);
 }
--- src/main/java/br/com/fiap/EnergyMonitor/service/SectorService.java
+++ src/main/java/br/com/fiap/EnergyMonitor/service/SectorService.java
@@ -1,13 +1,14 @@
 package br.com.fiap.EnergyMonitor.service;
 
+import br.com.fiap.EnergyMonitor.dto.CreateSectorDto;
 import br.com.fiap.EnergyMonitor.model.Sector;
-import br.com.fiap.EnergyMonitor.model.User;
 import br.com.fiap.EnergyMonitor.repository.SectorRepository;
 import br.com.fiap.EnergyMonitor.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class SectorService {
@@ -24,5 +25,44 @@ public class SectorService {
 
     public List<Sector> findAllByUserEmail(String userEmail) {
         return repository.findAllByUserEmail(userEmail);
+    }
+
+    public Optional<Sector> findById(Long id, String userEmail) {
+        Optional<Sector> sectorOptional = repository.findById(id);
+
+        if (sectorOptional.isPresent()) {
+            Sector sector = sectorOptional.get();
+            if (!sector.getUser().getEmail().equals(userEmail)) {
+                throw new RuntimeException("Sector dont belong to user");
+            }
+            return sectorOptional;
+        } else {
+            throw new RuntimeException("Sector not found!");
+        }
+    }
+
+    public Sector update(Long id, CreateSectorDto dto, String userEmail) {
+        Sector sector = repository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Sector not found!"));
+
+        if (!sector.getUser().getEmail().equals(userEmail)) {
+            throw new RuntimeException("Sector dont belong to user");
+        }
+
+        sector.setName(dto.name());
+        sector.setConsumptionLimit(dto.consumptionLimit());
+
+        return repository.save(sector);
+    }
+
+    public void delete(Long id, String userEmail) {
+        Sector sector = repository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Sector not found"));
+
+        if (!sector.getUser().getEmail().equals(userEmail)) {
+            throw new RuntimeException("Sector dont belong to user");
+        }
+
+        repository.delete(sector);
     }
 }
