1724cc93aa1ad84bc08064ae0f49be2ace142661
HumbertoVitalino <130328321+HumbertoVitalino@users.noreply.github.com>
2025-04-30 10:27:59 -0300

    feat: validation

--- src/main/java/br/com/fiap/EnergyMonitor/dto/CreateSectorDto.java
+++ src/main/java/br/com/fiap/EnergyMonitor/dto/CreateSectorDto.java
@@ -1,12 +1,15 @@
 package br.com.fiap.EnergyMonitor.dto;
 
+import jakarta.validation.constraints.DecimalMin;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 
 public record CreateSectorDto(
         @NotBlank(message = "Name is required")
         String name,
 
-        @NotBlank(message = "Limit is required")
+        @NotNull(message = "Limit is required")
+        @DecimalMin(value = "0.01", message = "Consumption limit must be greater than zero")
         Double consumptionLimit
 ) {
 }
